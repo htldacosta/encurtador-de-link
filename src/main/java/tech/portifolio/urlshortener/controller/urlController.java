880cf31581955c035a4e9a5b75a88aa91a45150c
htldacosta <hitaludcosta@gmail.com>
2025-11-02 13:27:28 -0400

    controller feito

---
+++ src/main/java/tech/portifolio/urlshortener/controller/urlController.java
@@ -0,0 +1,12 @@
+package tech.portifolio.urlshortener.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class urlController {
+
+    @PostMapping(value = "/shorten-url")
+    public ResponseEntity''
+}
---
+++ src/main/java/tech/portifolio/urlshortener/entities/Url.java
@@ -0,0 +1,9 @@
+package tech.portifolio.urlshortener.entities;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "urls")
+public class Url {
+
+    
+}
