22e9b045fa88d7bc894e233f6f4eeebcc450a83c
htldacosta <hitaludcosta@gmail.com>
2025-11-03 13:31:57 -0400

    Tests has begun. The object MockMvc, ObjectMapper, and the UrlRepository class have been inject into the test class.

--- src/test/java/tech/portifolio/urlshortener/UrlshortenerApplicationTests.java
+++ src/test/java/tech/portifolio/urlshortener/UrlshortenerApplicationTests.java
@@ -1,10 +1,31 @@
 package tech.portifolio.urlshortener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import tech.portifolio.urlshortener.repository.UrlRepository;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UrlshortenerApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private UrlRepository urlRepository;
+
+	@AfterEach
+	void tearDown() {
+		urlRepository.deleteAll();
+	}
 
 	@Test
 	void contextLoads() {
