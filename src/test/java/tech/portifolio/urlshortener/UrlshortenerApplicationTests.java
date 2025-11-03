5fa97032de1055acbbc52222207c184615b139e2
htldacosta <hitaludcosta@gmail.com>
2025-11-03 14:51:43 -0400

    The test class shouldShortenUrl was created.

--- src/test/java/tech/portifolio/urlshortener/UrlshortenerApplicationTests.java
+++ src/test/java/tech/portifolio/urlshortener/UrlshortenerApplicationTests.java
@@ -2,12 +2,24 @@ package tech.portifolio.urlshortener;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import tech.portifolio.urlshortener.controller.dto.ShortenUrlRequest;
 import tech.portifolio.urlshortener.repository.UrlRepository;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -25,6 +37,20 @@ class UrlshortenerApplicationTests {
 	@AfterEach
 	void tearDown() {
 		urlRepository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("Deve encurtar uma URL com sucesso")
+	void shouldShortenUrlSucessoFully() throws Exception {
+		var request = new ShortenUrlRequest("http://google.com.br");
+		var jsonRequest = objectMapper.writeValueAsString(request);
+
+		mockMvc.perform(post("/shorten-url")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(jsonRequest))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.shortUrl", is(notNullValue())));
+
 	}
 
 	@Test
